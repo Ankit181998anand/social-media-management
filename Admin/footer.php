a55b0327e9dcdc518e53fd70db4d99bad1df914f
Ankit181998anand <62587916+Ankit181998anand@users.noreply.github.com>
2024-10-02 00:11:24 +0530

    cleanup

--- Admin/footer.php
+++
@@ -1,8 +0,0 @@
-<!-- footer.php -->
-</main> <!-- Closing main section -->
-        </div> <!-- Closing row -->
-    </div> <!-- Closing container-fluid -->
-
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js"></script>
-</body>
-</html>
